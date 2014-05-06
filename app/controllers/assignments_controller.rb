d7841f1b51d52ee4c3c0760fb8ae1de08eddc0c4
halogenandtoast <halogenandtoast@gmail.com>
2014-05-05 21:58:52 -0400

    If it is not broke do not fix it

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,7 +1,13 @@
 class AssignmentsController < ApplicationController
   def update
-    box = current_user.boxes.find_by(params[:box_id])
-    box.update(location_id: params[:box][:location_id])
+    box = current_user.boxes.find(params[:id])
+    box.update(location: find_location)
     redirect_to dashboard_path
+  end
+
+  private
+
+  def find_location
+    current_user.locations.find_by_id(params[:box][:location_id])
   end
 end
