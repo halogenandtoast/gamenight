2f89e0abdd6ccfd597b7fc675548574341185522
halogenandtoast <halogenandtoast@gmail.com>
2014-05-07 11:08:28 -0400

    Ok

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,10 +5,14 @@ class Group < ActiveRecord::Base
   has_many :members, through: :group_memberships, class_name: "User", source: :user
   has_many :rsvps, -> (group) { where(date: group.next_date.to_date) }
   has_many :votes, -> (group) { where(voted_for: group.next_date.to_date) }
-  has_many :voted_games, through: :votes, source: :game, uniq: true
+  has_many :voted_games, through: :votes, source: :game
   has_many :rsvped_members, through: :rsvps, source: :user
   has_many :attending_rsvps, -> (group) { where("date = ? AND request != 'pass'", group.next_date.to_date) }, { class_name: 'Rsvp' }
   has_many :attendees, through: :attending_rsvps, source: :user
+
+  def games
+    voted_games.uniq
+  end
 
   def has_next_date?
     locations.any? { |location| location.has_next_date? }
