bae2e957400afc2f1b2952e7600728688ef9f0ad
halogenandtoast <halogenandtoast@gmail.com>
2014-05-07 11:03:47 -0400

    Uniq not unique

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,7 +5,7 @@ class Group < ActiveRecord::Base
   has_many :members, through: :group_memberships, class_name: "User", source: :user
   has_many :rsvps, -> (group) { where(date: group.next_date.to_date) }
   has_many :votes, -> (group) { where(voted_for: group.next_date.to_date) }
-  has_many :voted_games, through: :votes, source: :game, unique: true
+  has_many :voted_games, through: :votes, source: :game, uniq: true
   has_many :rsvped_members, through: :rsvps, source: :user
   has_many :attending_rsvps, -> (group) { where("date = ? AND request != 'pass'", group.next_date.to_date) }, { class_name: 'Rsvp' }
   has_many :attendees, through: :attending_rsvps, source: :user
