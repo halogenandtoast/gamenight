ec13d75f502a6835519a81fbb80f7f67007273fc
halogenandtoast <halogenandtoast@gmail.com>
2014-05-19 23:40:55 -0400

    Do not import without username

--- app/controllers/bgg_imports_controller.rb
+++ app/controllers/bgg_imports_controller.rb
@@ -1,8 +1,10 @@
 class BggImportsController < ApplicationController
   def create
     bgg_username = params[:bgg_import][:bgg_username]
-    current_user.update(bgg_username: bgg_username)
-    BggImporter.new(current_user).import
-    redirect_to dashboard_path
+    if bgg_username
+      current_user.update(bgg_username: bgg_username)
+      BggImporter.new(current_user).import
+      redirect_to dashboard_path
+    end
   end
 end
